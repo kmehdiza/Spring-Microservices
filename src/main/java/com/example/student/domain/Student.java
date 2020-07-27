4e6d167fff42fc243ebd4bb80551f00f5208ab6e
Kanan <mehdizada.kanan@gmail.com>
2020-07-28 00:47:26 +0200

    GenerationType.IDENTITY is added

--- src/main/java/com/example/student/domain/Student.java
+++ src/main/java/com/example/student/domain/Student.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Data
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
